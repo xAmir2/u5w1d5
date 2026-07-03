0ca72999fccaec43901a42009783a9020a5d5ebd
Amir B. Kahna <amir.kahna2@gmail.com>
2026-07-03 09:49:18 +0200

    Initial commit

---
+++ src/test/java/amirka/u5w1d5/U5w1d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package amirka.u5w1d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w1d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
